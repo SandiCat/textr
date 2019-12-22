6af03b9b779cf5ee2e008718e52a3c9483930292
Sandi <dsandi333@gmail.com>
2019-12-22 02:55:08 +0100

    add test data to the db

---
+++ timesafe-backend/sql/create_schema.sql
@@ -0,0 +1,5 @@
+create table fruit (
+    id serial primary key,
+    name text not null,
+    sugar_content float
+);
---
+++ timesafe-backend/sql/populate_db.sql
@@ -0,0 +1,6 @@
+insert into fruit 
+    (name, sugar_content)
+values
+    ('apple', 0.213),
+    ('orange', 3.211),
+    ('banana', 34.323);
