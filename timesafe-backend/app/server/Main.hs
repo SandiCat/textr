35944eb697216180f4b39b6cfef9c99d87a96336
Sandi <dsandi333@gmail.com>
2020-02-20 20:43:32 +0100

    createschema instead of migrating

--- timesafe-backend/app/server/Main.hs
+++ timesafe-backend/app/server/Main.hs
@@ -1,59 +1,60 @@
-module Main (main) where
+{-# LANGUAGE TypeApplications #-}
 
-import Servant.Server
-import qualified Network.Wai.Handler.Warp as Warp
-import qualified Database.Beam.Postgres as Beam
-import qualified Control.Exception as Exception 
-import qualified Data.Aeson as Aeson
-import           System.Environment             ( getArgs )
-import           Control.Concurrent (threadDelay)
-import qualified Database.Postgres.Temp as PgTemp
-import qualified Database.PostgreSQL.Simple as Pg
-import System.FilePath ((</>))
+module Main
+  ( main,
+  )
+where
 
 import qualified API
+import Control.Concurrent (threadDelay)
+import qualified Control.Exception as Exception
+import qualified Data.Aeson as Aeson
 import Database.Beam.Migrate.Simple
-import qualified MonadStack
+import qualified Database.Beam.Postgres as Beam
 import Database.Beam.Postgres.Migrate (migrationBackend)
+import qualified Database.PostgreSQL.Simple as Pg
+import qualified Database.Postgres.Temp as PgTemp
 import qualified Migration
+import qualified MonadStack
+import qualified Network.Wai.Handler.Warp as Warp
+import Servant.Server
+import System.Environment (getArgs)
+import System.FilePath ((</>))
 
 -- ugly hack until i learn more about exceptions
 keepTrying :: IO a -> IO a
 keepTrying action =
-    Exception.catch @Exception.SomeException
-        action
-        $ \e -> do
-            print e
-            threadDelay $ 2_000_000
-            keepTrying action
+  Exception.catch @Exception.SomeException
+    action
+    $ \e -> do
+      print e
+      threadDelay $ 2_000_000
+      keepTrying action
 
 instance Aeson.FromJSON Beam.ConnectInfo
 
 withConfigFile :: FilePath -> IO ()
 withConfigFile configPath = do
-    Just connInfo <- Aeson.decodeFileStrict' @Beam.ConnectInfo configPath
-    conn <- keepTrying $ Beam.connect $ connInfo
-    putStrLn "server running"
-    Warp.run 8080 $ serve API.apiProxy $ MonadStack.hoistedServer conn
-
+  Just connInfo <- Aeson.decodeFileStrict' @Beam.ConnectInfo configPath
+  conn <- keepTrying $ Beam.connect $ connInfo
+  putStrLn "server running"
+  Warp.run 8080 $ serve API.apiProxy $ MonadStack.hoistedServer conn
 
 executeSqlFile :: Pg.Connection -> FilePath -> IO Int64
 executeSqlFile conn path = do
-    queryText <- readFile path
-    Pg.execute_ conn $ fromString queryText
+  queryText <- readFile path
+  Pg.execute_ conn $ fromString queryText
 
 develMain :: IO (Either PgTemp.StartError ())
 develMain =
-    PgTemp.with $ \db -> do
-        conn <- Pg.connectPostgreSQL $ PgTemp.toConnectionString db
-        Beam.runBeamPostgres conn $ autoMigrate migrationBackend Migration.migrationDb
-        
-        _ <- executeSqlFile conn $ "sql" </> "populate_db.sql"
-
-        putStrLn "server running"
-        Warp.run 8080 $ serve API.apiProxy $ MonadStack.hoistedServer conn
+  PgTemp.with $ \db -> do
+    conn <- Pg.connectPostgreSQL $ PgTemp.toConnectionString db
+    Beam.runBeamPostgres conn $ createSchema migrationBackend Migration.migrationDb
+    _ <- executeSqlFile conn $ "sql" </> "populate_db.sql"
+    putStrLn "server running"
+    Warp.run 8080 $ serve API.apiProxy $ MonadStack.hoistedServer conn
 
 main :: IO ()
 main = do
-    [configPath] <- getArgs
-    withConfigFile configPath
+  [configPath] <- getArgs
+  withConfigFile configPath
