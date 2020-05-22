54dc3893a4b46b0f3d58ea23394f3f2689a639e1
Sandi <dsandi333@gmail.com>
2020-05-22 23:09:35 +0200

    fromat types

--- timesafe-backend/src/Types.hs
+++ timesafe-backend/src/Types.hs
@@ -4,67 +4,71 @@
 module Types where
 
 import qualified Data.Aeson as Aeson
-import qualified Generics.SOP as SOP
-import           Database.Beam.Backend.SQL
-import           Database.Beam.Migrate          ( HasDefaultSqlDataType
-                                                , defaultSqlDataType
-                                                )
-import qualified Database.Beam.Backend.SQL as SQL 
+import Database.Beam.Backend.SQL
+import qualified Database.Beam.Backend.SQL as SQL
+import Database.Beam.Migrate
+  ( HasDefaultSqlDataType,
+    defaultSqlDataType,
+  )
 import Database.Beam.Postgres (Postgres)
+import qualified Generics.SOP as SOP
 
 data Sex
-    = Male
-    | Female
-    deriving (Show, Read, Enum, Bounded, Eq, Generic, SOP.Generic, SOP.HasDatatypeInfo)
+  = Male
+  | Female
+  deriving (Show, Read, Enum, Bounded, Eq, Generic, SOP.Generic, SOP.HasDatatypeInfo)
 
 instance Aeson.ToJSON Sex
+
 instance Aeson.FromJSON Sex
 
 -- perhaps it would be better to marshal it using postgres' support for json, but this is simpler
 instance HasSqlValueSyntax be String => HasSqlValueSyntax be Sex where
-    sqlValueSyntax = autoSqlValueSyntax
+  sqlValueSyntax = autoSqlValueSyntax
 
 instance HasDefaultSqlDataType Postgres Sex where
-    defaultSqlDataType _ _ _ = SQL.varCharType Nothing Nothing
+  defaultSqlDataType _ _ _ = SQL.varCharType Nothing Nothing
 
 instance FromBackendRow Postgres Sex where
-    fromBackendRow = fromMaybe (error "invalid format") . readMaybe <$> fromBackendRow
-    -- docs say this function can't fail so i'm using error. the guide throws an exception as well
+  fromBackendRow = fromMaybe (error "invalid format") . readMaybe <$> fromBackendRow
 
+-- docs say this function can't fail so i'm using error. the guide throws an exception as well
 
 data Gender
-    = Cis Sex
-    | TransTo Sex
-    | Other (Maybe Text) -- | an alternative gender could be named arbitrarily or be left unspecified
-    deriving (Show, Read, Eq, Generic, SOP.Generic, SOP.HasDatatypeInfo)
+  = Cis Sex
+  | TransTo Sex
+  | Other (Maybe Text) -- an alternative gender could be named arbitrarily or be left unspecified
+  deriving (Show, Read, Eq, Generic, SOP.Generic, SOP.HasDatatypeInfo)
 
 instance Aeson.ToJSON Gender
+
 instance Aeson.FromJSON Gender
 
 instance HasSqlValueSyntax be String => HasSqlValueSyntax be Gender where
-    sqlValueSyntax = autoSqlValueSyntax
+  sqlValueSyntax = autoSqlValueSyntax
 
 instance HasDefaultSqlDataType Postgres Gender where
-    defaultSqlDataType _ _ _ = SQL.varCharType Nothing Nothing
+  defaultSqlDataType _ _ _ = SQL.varCharType Nothing Nothing
 
 instance FromBackendRow Postgres Gender where
-    fromBackendRow =
-        fromMaybe (error "invalid format") . readMaybe <$> fromBackendRow
+  fromBackendRow =
+    fromMaybe (error "invalid format") . readMaybe <$> fromBackendRow
 
 data Choice
-    = Accepted
-    | Declined
-    deriving (Show, Read, Enum, Bounded, Eq, Generic, SOP.Generic, SOP.HasDatatypeInfo)
+  = Accepted
+  | Declined
+  deriving (Show, Read, Enum, Bounded, Eq, Generic, SOP.Generic, SOP.HasDatatypeInfo)
 
 instance Aeson.ToJSON Choice
+
 instance Aeson.FromJSON Choice
 
 instance HasSqlValueSyntax be String => HasSqlValueSyntax be Choice where
-    sqlValueSyntax = autoSqlValueSyntax
+  sqlValueSyntax = autoSqlValueSyntax
 
 instance HasDefaultSqlDataType Postgres Choice where
-    defaultSqlDataType _ _ _ = SQL.varCharType Nothing Nothing
+  defaultSqlDataType _ _ _ = SQL.varCharType Nothing Nothing
 
 instance FromBackendRow Postgres Choice where
-    fromBackendRow =
-        fromMaybe (error "invalid format") . readMaybe <$> fromBackendRow
+  fromBackendRow =
+    fromMaybe (error "invalid format") . readMaybe <$> fromBackendRow
